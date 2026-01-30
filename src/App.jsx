517034be1bee3184ddcc7fe56a3d0c4c550ee5dc
HtunTaukOo <htuntaukoo86@gmail.com>
2026-01-30 14:09:43 +0700

    fix1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import SalesJournal from "./pages/SalesJournal";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/journal" element={<SalesJournal />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
